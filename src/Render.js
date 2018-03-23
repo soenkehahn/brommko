03278de45525b55704484bf06f5a910ecab29633
Sönke Hahn <soenkehahn@gmail.com>
2018-03-23 18:00:22 -0400

    put player in the center

--- src/Render.js
+++ src/Render.js
@@ -3,10 +3,34 @@
 import React, { Component } from "react";
 
 export class Render extends Component<{}> {
+  svgWidth: number;
+  svgHeight: number;
+
+  constructor() {
+    super();
+    this.svgWidth = 800;
+    this.svgHeight = 500;
+  }
+
+  transformX(x: number): number {
+    return this.svgWidth / 2 + x;
+  }
+
+  transformY(y: number): number {
+    return this.svgHeight / 2 + y;
+  }
+
   render() {
+    const size = 40;
     return (
-      <svg>
-        <rect />
+      <svg width={this.svgWidth} height={this.svgHeight}>
+        <rect
+          x={this.transformX(0)}
+          y={this.transformY(0)}
+          width={size}
+          height={size}
+          fill="green"
+        />
       </svg>
     );
   }
